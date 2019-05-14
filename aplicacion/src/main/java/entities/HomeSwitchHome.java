335538273be93271ce2a1d85ac66ca727d66ca74
nico_ <nico_@DESKTOP-DOA2B3I>
2019-05-14 18:47:16 -0300

    Configuracion de la base de datos, primer commit

---
+++ aplicacion/src/main/java/entities/HomeSwitchHome.java
@@ -0,0 +1,22 @@
+package entities;
+
+public class HomeSwitchHome {
+	 int num=8;
+	 public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	String nombre;
+
+	public int getNum() {
+		return num;
+	}
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+}
---
+++ aplicacion/src/main/java/entities/Propiedad.java
@@ -0,0 +1,79 @@
+package entities;
+
+
+
+public class Propiedad  {
+ /**
+	 * 
+	 */
+	private long id;
+	public long getId() {
+	return id;
+}
+public void setId(long id) {
+	this.id = id;
+}
+public static long getSerialversionuid() {
+	return serialVersionUID;
+}
+	private static final long serialVersionUID = 1L;
+public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getPais() {
+		return pais;
+	}
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+	public String getProvincia() {
+		return provincia;
+	}
+	public void setProvincia(String provincia) {
+		this.provincia = provincia;
+	}
+	public String getLocalidad() {
+		return localidad;
+	}
+	public void setLocalidad(String localidad) {
+		this.localidad = localidad;
+	}
+	public String getDomicilio() {
+		return domicilio;
+	}
+	public void setDomicilio(String domicilio) {
+		this.domicilio = domicilio;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public int getMontoBase() {
+		return montoBase;
+	}
+	public void setMontoBase(int montoBase) {
+		this.montoBase = montoBase;
+	}
+private  String titulo,pais,provincia,localidad,domicilio,descripcion;
+//falta foto de tipo image
+ private int montoBase;
+public Propiedad(String titulo, String pais, String provincia, String localidad, String domicilio, String descripcion,
+		int montoBase) {
+	super();
+	this.titulo = titulo;
+	this.pais = pais;
+	this.provincia = provincia;
+	this.localidad = localidad;
+	this.domicilio = domicilio;
+	this.descripcion = descripcion;
+	this.montoBase = montoBase;
+}
+public Propiedad() {
+	
+}
+}
