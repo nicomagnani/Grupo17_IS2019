e18ecb91a8a58ca732bad773c77f02674b284c59
nicomagnani <48930777+nicomagnani@users.noreply.github.com>
2019-05-16 19:47:08 -0300

    commit prueba

---
+++ Downloads/Grupo17_IS2019-master/Grupo17_IS2019-master/homeSwitchHome/src/main/java/homeSwitchHome/Usuario.java
@@ -0,0 +1,31 @@
+package homeSwitchHome;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Usuario {
+
+	public Usuario() {	//crearUsuario(), tal vez lo borremos
+		super();
+		// TODO Auto-generated method stub
+	}
+
+	private String mail;
+	private List<Reserva> reservas = new ArrayList<Reserva>();
+	
+	public void realizarReserva(Propiedad unaPropiedad, int unaSemana)
+	{
+		
+	}
+	
+	public List<Propiedad> verResidencias()
+	{
+		return null;
+		//TODO
+	}
+	
+	public void ofertarSubasta(ReservaSubasta unaSubasta, int unaCantidad) 
+	{
+		
+	}
+}
---
+++ Downloads/Grupo17_IS2019-master/Grupo17_IS2019-master/homeSwitchHome/src/main/java/homeSwitchHome/UsuarioAdministrador.java
@@ -0,0 +1,25 @@
+package homeSwitchHome;
+
+import java.awt.List;
+
+public class UsuarioAdministrador extends Usuario {
+
+	public UsuarioAdministrador() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	private String contraseña;
+	
+	private Propiedad agregarResidencia() {
+		return null;
+	} //TODO
+	
+	private void modificarResidencia() {
+	} //TODO
+	
+	//private List<Reserva> verReservas()
+	//{
+	//	return null;
+//	}
+}
