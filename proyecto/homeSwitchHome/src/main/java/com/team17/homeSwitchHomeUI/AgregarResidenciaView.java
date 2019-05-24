da109de6f356d4c5f2c86ae92950d6b06ad6fa3f
leandroll1 <leandro_llorente@outlook.com>
2019-05-24 14:39:55 -0300

    añadido campo para monto base al agregar propiedad (2)

--- proyecto/homeSwitchHome/src/main/java/com/team17/homeSwitchHomeUI/AgregarResidenciaView.java
+++ proyecto/homeSwitchHome/src/main/java/com/team17/homeSwitchHomeUI/AgregarResidenciaView.java
@@ -5,6 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import org.vaadin.ui.NumberField;
+
+import com.vaadin.data.Binder;
 import com.vaadin.navigator.View;
 import com.vaadin.shared.ui.ValueChangeMode;
 import com.vaadin.ui.AbstractTextField;
@@ -18,57 +21,56 @@ import com.vaadin.ui.TextField;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.themes.ValoTheme;
 
+import homeSwitchHome.Propiedad;
+
 public class AgregarResidenciaView extends Composite implements View {  //.necesita composite y view para funcionar correctamente
 	
-	FormLayout formulario = new FormLayout();;
+	FormLayout formulario = new FormLayout();
 	TextField titulo = new TextField("Título");
 	TextArea descripcion = new TextArea("Descripción");
 	TextField pais = new TextField("País");
 	TextField provincia = new TextField("Provincia");
 	TextField localidad = new TextField("Localidad");
 	TextField domicilio = new TextField("Domicilio");
+	NumberField monto = new NumberField("Monto base");
 	//TODO: añadir campo para cargar imagenes		
 	Button aceptarButton = new Button("Aceptar");
 	Label resultado1 = new Label();
 	Label resultado2 = new Label();
-	boolean cumple1 = false;
-	boolean cumple2 = false;
-	boolean cumple3 = false;
-	boolean cumple4 = false;
-	boolean cumple5 = false;
-	boolean cumple6 = false;
-	boolean cumpleTodo = false;
+	
+	
+	boolean[] cumple = {false,false,false,false,false,false,false,false}; //el ultimo elemento es true cuando se completen todos los campos
 	
 	public AgregarResidenciaView() {
-		
-		cumple1 = cumple2 = cumple3 = cumple4 = cumple5 = cumple6 = false;
-		
-		titulo.addValueChangeListener(e1 -> esVacio1(titulo.getValue()));
+		titulo.addValueChangeListener(e1 -> esVacio(titulo.getValue(),0));
 		titulo.setValueChangeMode(ValueChangeMode.EAGER);
 		
-		descripcion.addValueChangeListener(e2 -> esVacio2(descripcion.getValue()));
+		descripcion.addValueChangeListener(e2 -> esVacio(descripcion.getValue(),1));
 		descripcion.setValueChangeMode(ValueChangeMode.EAGER);
 		
-		pais.addValueChangeListener(e3 -> esVacio3(pais.getValue()));
+		pais.addValueChangeListener(e3 -> esVacio(pais.getValue(),2));
 		pais.setValueChangeMode(ValueChangeMode.EAGER);
 		
-		provincia.addValueChangeListener(e4 -> esVacio4(provincia.getValue()));
+		provincia.addValueChangeListener(e4 -> esVacio(provincia.getValue(),3));
 		provincia.setValueChangeMode(ValueChangeMode.EAGER);
 		
-		localidad.addValueChangeListener(e5 -> esVacio5(localidad.getValue()));
+		localidad.addValueChangeListener(e5 -> esVacio(localidad.getValue(),4));
 		localidad.setValueChangeMode(ValueChangeMode.EAGER);
 		
-		domicilio.addValueChangeListener(e6 -> esVacio6(domicilio.getValue()));
-		domicilio.setValueChangeMode(ValueChangeMode.EAGER);
+		domicilio.addValueChangeListener(e6 -> esVacio(domicilio.getValue(),5));
+		domicilio.setValueChangeMode(ValueChangeMode.EAGER);		
+
+		monto.addValueChangeListener(e7 -> esVacio(monto.getValue(),6));
+		monto.setValueChangeMode(ValueChangeMode.EAGER);
 		
-		aceptarButton.addClickListener(e7 -> {
+		aceptarButton.addClickListener(e -> {
 			try {
 				aceptar();
-			} catch (SQLException e) {
+			} catch (SQLException e8) {
 				// TODO Auto-generated catch block
-				e.printStackTrace();
+				e8.printStackTrace();
 			}
-		});	
+		});
 		
 		formulario.addComponent(titulo);
 		formulario.addComponent(descripcion);
@@ -76,60 +78,29 @@ public class AgregarResidenciaView extends Composite implements View {  //.neces
 		formulario.addComponent(provincia);
 		formulario.addComponent(localidad);
 		formulario.addComponent(domicilio);
+		formulario.addComponent(monto);		
 		//formulario.addComponent(campo donde se cargan imagenes);
 		formulario.addComponent(aceptarButton);
 		formulario.addComponent(resultado1);
 		formulario.addComponent(resultado2);
-		
-			
 		
 		VerticalLayout mainLayout = new VerticalLayout(formulario);
 		
 		setCompositionRoot(mainLayout);
     }
 	
-	private void esVacio1(String st) {
-		if (st == "")
-			cumple1 = false;
-		else cumple1 = true;
-	}
-
-	private void esVacio2(String st) {
-		if (st == "")
-			cumple2 = false;
-		else cumple2 = true;
-	}
-	
-	private void esVacio3(String st) {
-		if (st == "")
-			cumple3 = false;
-		else cumple3 = true;
-	}
-	
-	private void esVacio4(String st) {
-		if (st == "")
-			cumple4 = false;
-		else cumple4 = true;
-	}
-	
-	private void esVacio5(String st) {
-		if (st == "")
-			cumple5 = false;
-		else cumple5 = true;
-	}
-	
-	private void esVacio6(String st) {
-		if (st == "")
-			cumple6 = false;
-		else cumple6 = true;
+	private void esVacio(String st, int i) {
+		if (st == null)
+			cumple[i] = false;
+		else cumple[i] = true;
 	}
 	
 	private void aceptar() throws SQLException {
-		cumpleTodo = true;
+		cumple[7] = true;
 		
-		if ((!cumple1) || (!cumple2) || (!cumple3) || (!cumple4) || (!cumple5) || (!cumple6)) {
+		if ((!cumple[0]) || (!cumple[1]) || (!cumple[1]) || (!cumple[3]) || (!cumple[4]) || (!cumple[5]) || (!cumple[6])) {
 			resultado1.setValue("Error: Deben completarse todos los campos.");
-			cumpleTodo = false;
+			cumple[7] = false;
 		}
 		
 		//TODO: chequear si se subieron max 5 imagenes
@@ -139,9 +110,10 @@ public class AgregarResidenciaView extends Composite implements View {  //.neces
 		//}
 		
 		//si cumple todos los requisitos, cargo la residencia y borro el formulario
-		if (cumpleTodo) {
+		if (cumple[7] = true) {
 			ConnectionBD con = new ConnectionBD();
-			con.agregarResidencia(titulo.getValue(), descripcion.getValue(), pais.getValue(), provincia.getValue(), localidad.getValue(), domicilio.getValue());
+			con.agregarResidencia(titulo.getValue(), descripcion.getValue(), pais.getValue(), provincia.getValue(), localidad.getValue(),
+					domicilio.getValue(),Integer.parseInt(monto.getValue()));
 			resultado1.setValue("Éxito.");
 			for ( Component comp : formulario ) {
 			    if (comp instanceof AbstractTextField)
