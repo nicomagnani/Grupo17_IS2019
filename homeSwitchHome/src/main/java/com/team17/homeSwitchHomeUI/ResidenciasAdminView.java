76afee8c17b290bd9811dfbf8c30d30dd9898efe
frang <frang@192.168.0.146>
2019-06-11 17:02:48 -0300

    Pequeño cambio de diseño en ResidenciasAdminView

--- homeSwitchHome/src/main/java/com/team17/homeSwitchHomeUI/ResidenciasAdminView.java
+++ homeSwitchHome/src/main/java/com/team17/homeSwitchHomeUI/ResidenciasAdminView.java
@@ -3,25 +3,26 @@ package com.team17.homeSwitchHomeUI;
 import java.sql.SQLException;
 import java.util.Iterator;
 
+import com.vaadin.annotations.Theme;
 import com.vaadin.navigator.View;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.Composite;
+import com.vaadin.ui.CssLayout;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.Panel;
 import com.vaadin.ui.VerticalLayout;
 
 import homeSwitchHome.Propiedad;
 
 public class ResidenciasAdminView extends Composite implements View {
-	
 	VerticalLayout mainLayout;
 		
 	public ResidenciasAdminView() {
 		
 		mainLayout = new VerticalLayout();
 		this.cargarResidencias();
-        mainLayout.setSizeFull();
         setCompositionRoot((Component) mainLayout);
 	}
 
@@ -55,11 +56,16 @@ public class ResidenciasAdminView extends Composite implements View {
 				unaResidencia.getProvincia() + "," + unaResidencia.getLocalidad() +
 				"," + unaResidencia.getDomicilio());
 		Label descripcion = new Label (unaResidencia.getDescripcion());
-		//String monto = String.valueOf(unaResidencia.getMontoBase());.-
 		Label montoBase = new Label ("monto base:" + String.valueOf(unaResidencia.getMontoBase()));
 		
 		VerticalLayout residenciaLayout = new VerticalLayout(titulo,ubicacion,descripcion,montoBase,botones);
-		mainLayout.addComponent(residenciaLayout);
+		residenciaLayout.setSizeFull();
+		residenciaLayout.setStyleName("lightBackground");
+		
+		Panel residenciaPanel = new Panel();
+		residenciaPanel.setContent(residenciaLayout);
+		
+		mainLayout.addComponent(residenciaPanel);
 		
 	}
 	
